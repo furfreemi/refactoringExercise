0db7fedd9ae62db30ff5ca90c62952f16cc3b32c
compwron <wordymusic@gmail.com>
2013-07-17 22:24:48 -0500

    Adding complete starter code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Exercise taken from: http://patrickwilsonwelsh.com/?p=18
+The real starter code is here https://ugly-and-clean-tictactoe.googlecode.com/svn/trunk/TicTacToe-SelfContained-Ant/
+Along with a finished solution.
 
 Goal: refactor this to make it more maintainable and extensible
--- Refactoring/test/exercise/refactoring/TicTacToeTest.java
+++ Refactoring/test/exercise/refactoring/TicTacToeTest.java
@@ -2,13 +2,9 @@ package exercise.refactoring;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.fail;
-
 public class TicTacToeTest {
     @Test
-    public void shouldFail(){
-        fail("stuff");
-        assertEquals(1, );
+    public void shouldRun(){
+        TicTacToe ticTacToe = new TicTacToe();
     }
 }
