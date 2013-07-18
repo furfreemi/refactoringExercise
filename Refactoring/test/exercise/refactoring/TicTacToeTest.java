f4778831f014463eb1204541ecb8cf3c9daf8129
compwron <wordymusic@gmail.com>
2013-07-17 22:04:44 -0500

    Gradle project and test code; starter code and test are failing compile

---
+++ Refactoring/test/exercise/refactoring/TicTacToeTest.java
@@ -0,0 +1,14 @@
+package exercise.refactoring;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
+
+public class TicTacToeTest {
+    @Test
+    public void shouldFail(){
+        fail("stuff");
+        assertEquals(1, );
+    }
+}
